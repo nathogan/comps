6103a085e34a2e48da92d91acca187967c0bd746
nathogan <89811214+nathogan@users.noreply.github.com>
2024-09-19 17:47:11 -0700

    adding testing

--- test.py
+++ test.py
@@ -155,13 +155,54 @@ if __name__ == '__main__':
         print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
     #print(enumerate(train_dataloader))
-
+    '''
     epochs = 5
     for t in range(epochs):
         print(f"Epoch {t+1}\n-------------------------------")
         train(train_dataloader, model, loss_fn, optimizer)
         test(test_dataloader, model, loss_fn)
     print("Done!")
+    '''
+    #torch.save(model.state_dict(), "test_model_5_epoch.pth")
+    #print("Saved PyTorch Model State to test_model_5_epoch.pth")
 
-    torch.save(model.state_dict(), "test_model_5_epoch.pth")
-    print("Saved PyTorch Model State to test_model_5_epoch.pth")
+
+        
+    model = NeuralNetwork().to(device)
+    model.load_state_dict(torch.load("test_model_5_epoch.pth", weights_only=True))
+
+    classes = training_data.classes
+    #print(classes[0])
+
+
+    model.eval()
+    #print(test_dataloader)
+    with torch.no_grad():
+            for X, y in test_dataloader:
+                #print(X.size())
+                X, y = X.to(device), y.to(device)
+                pred = model(X)
+                #print(pred)
+                #print(y)
+                predicted, actual = classes[pred[0].argmax(0)], classes[y]
+                print(f'Predicted: "{predicted}", Actual: "{actual}"')
+                
+                
+    '''
+    x, y = testing_data[0][0], testing_data[0][1]
+    #print(testing_data[0][0])
+    #print(testing_data[0][1])
+    #m = nn.Flatten()
+    with torch.no_grad():
+        #x.unsqueeze(0)
+        #print(x.size())
+        
+        x = x.to(device)
+        x = torch.flatten(x, start_dim=1)
+        #x = x.flatten()
+        #print(x.size())
+        pred = model(x)
+        predicted, actual = classes[pred[0].argmax(0)], classes[y]
+        print(f'Predicted: "{predicted}", Actual: "{actual}"')
+    '''
+    
